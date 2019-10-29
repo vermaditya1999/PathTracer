095c94b3e1c2c34dbb27b8472e131b7d2d80885b
Pr0-N00b <rishi17260@iiitd.ac.in>
2019-10-29 18:36:14 +0530

    PPM done XD

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include <cstdio>
 
-
-
-void ppm_image(std::vector<int> &data , std::string file_name, int n , int m)
+void ppm_image(std::vector<std::vector<int>> &data , std::string file_name, int n , int m)
 {
     FILE *fp = fopen(file_name.c_str() , "w");
     if (!fp){
@@ -12,31 +10,24 @@ void ppm_image(std::vector<int> &data , std::string file_name, int n , int m)
         perror(" ");
         return;
     }
-    if (data.size() != 3*n*m){
+    if (data.size() != n*m){
         std::cout << "Data not complete to print image" << std::endl;
         return;
     }
 
-    std::fprintf(fp , "P3\n%d  %d\n255\n" , m , n);
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            fprintf(fp, "%c %c %c ", data[i*m+n], data[i*m+n+1], data[i*m+n+2]);
-        }
+    fprintf(fp , "P3\n%d  %d\n255\n" , m , n);
+    for(int i=0; i<n*m ;i+=1){
+        fprintf(fp,"%d %d %d " , data[i][0] , data[i][1] , data[i][2]);
     }
 }
 
 int main()
 {
     std::cout << "PathTracer" << std::endl;
-    std::vector<int> image;
-    for(int i=0; i<256; i++){
-        for(int j=0; j<256; j++){
-            image.push_back(255);
-            image.push_back(0);
-            image.push_back(0);
-        }
+    std::vector<std::vector<int>> image;
+    for(int i=0; i<256*256; i+=1){
+        std::vector<int> v = {255 , 0 , 0};
+        image.push_back(v);
     }
     ppm_image(image , "file.ppm" , 256 , 256);
 }
