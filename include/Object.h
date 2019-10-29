171652139b8137725d59000c679506f6591130d8
Aditya Verma <aditya17009@iiitd.ac.in>
2019-10-29 20:35:04 +0530

    Added Object class.

---
+++ include/Object.h
@@ -0,0 +1,18 @@
+#ifndef PATHTRACER_OBJECT_H
+#define PATHTRACER_OBJECT_H
+
+
+#include "vec3.h"
+#include "Ray.h"
+
+
+class Object {
+private:
+    vec3 position;
+
+public:
+    // TODO: Create intersect method
+};
+
+
+#endif
