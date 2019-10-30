311a374aa2782c504d29eea7b0aeb4d3722777f7
Aditya Verma <aditya17009@iiitd.ac.in>
2019-10-30 15:56:04 +0530

    Create Options class.

---
+++ include/Options.h
@@ -0,0 +1,39 @@
+#ifndef PATHTRACER_OPTIONS_H
+#define PATHTRACER_OPTIONS_H
+
+
+#include <string>
+#include <cstring>
+
+#define usage() fprintf(stderr , "./PathTracer --file <filename>\n")
+
+class Options {
+private:
+    int argc;
+    char **argv;
+
+public:
+    std::string filename;
+
+    Options(int _argc, char *_argv[]) : argc(_argc), argv(_argv) {}
+
+    void parse();
+};
+
+void Options::parse() {
+    if (argc == 1) {
+        usage();
+        exit(-1);
+    }
+    for (int i = 1; i < argc; ++i) {
+        if (!strcmp(argv[i], "--file")) {
+            filename = argv[++i];
+        } else {
+            usage();
+            exit(-1);
+        }
+    }
+}
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,10 @@
-#include <iostream>
 #include <vector>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
 
-#include "debug.h"
+#include "Options.h"
 
-
-class Options{
-public:
-    std::string filename;
-    Options(int _argc , char *_argv[]): argc(_argc), argv(_argv)
-    {}
-    void parse();
-private:
-    int argc;
-    char **argv;
-
-};
-
-#define usage() fprintf(stderr , "./PathTracer --file <filename>\n")
-
-void Options::parse()
-{
-    if (argc==1){
-        usage();
-        exit(-1);
-    }
-    for(int  i=1 ; i<argc ; i++){
-        if (!strcmp(argv[i] , "--file")){
-            filename = argv[++i];
-        }
-        else{
-            usage();
-            exit(-1);
-        }
-    }
-}
 
 int main(int argc , char *argv[])
 {
-
     Options options(argc , argv);
     options.parse();
 
