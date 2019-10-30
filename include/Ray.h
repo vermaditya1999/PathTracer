62ae26bb725364bce04fb2fdb6a194e44e889a04
Aditya Verma <aditya17009@iiitd.ac.in>
2019-10-30 11:33:11 +0530

    Update Ray class.

--- include/Ray.h
+++ include/Ray.h
@@ -3,6 +3,7 @@
 
 
 #include "vec3.h"
+#include "Object.h"
 
 
 class Ray {
@@ -10,10 +11,24 @@ private:
     vec3 origin;
     vec3 direction;
     double t;  // Parameter
+    Object *intersectedObject;
 
 public:
     explicit Ray(const vec3 _origin, const vec3 _direction, double _t) :
-            origin(_origin), direction(_direction), t(_t) {}
+            origin(_origin), direction(_direction), t(_t), intersectedObject(nullptr) {
+
+    }
+
+    // Returns true if ray has intersected an object.
+    bool intersected();
+
+    // Returns the pointer to the intersected object.
+    // Returns nullptr if ray has not intersected object until now.
+    Object *getIntersectedObject();
+
+    // Set the ray parameter and intersected object if this is the
+    // closest hit in the positive direction of ray.
+    void setParameter(Object *object, double _t);
 };
 
 
