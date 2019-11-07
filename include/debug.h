cea162f257654afd26f5ac903947bd09d381ea0f
Aditya Verma <aditya17009@iiitd.ac.in>
2019-11-08 01:01:38 +0530

    Set default build to Release.

--- include/debug.h
+++ include/debug.h
@@ -1,15 +1,14 @@
 #ifndef PATHTRACER_DEBUG_H
 #define PATHTRACER_DEBUG_H
 
-
 #include <cstdio>
 
-#ifndef NDEBUG
+#ifdef DEBUG
 #define debug(fmt , ...) do {\
     fprintf(stderr , "[%s:%s():%d] ", __FILE__ , __func__ , __LINE__  );\
     fprintf(stderr , fmt , __VA_ARGS__); \
     fputs("\n", stderr); \
-}while(0)
+} while(0)
 #else
 #define debug(fmt , ...)
 #endif
